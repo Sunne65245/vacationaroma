66b20e2cc6b9d95d49bfa7e51efd1664857fccb1
sunne <sun65245@gmail.com>
2021-04-14 16:26:03 +0800

    完成後台介面、購物車、登入成功

--- js/signUp.js
+++ js/signUp.js
@@ -1,29 +1,22 @@
-let API = `http://vacationaroma.rocket-coding.com/api/Login`;
+let API = `http://vacationaroma.rocket-coding.com/api/User/PostUser`;
 
 const MyToken = "";
 const license = { headers: { Authorization: `Bearer ${MyToken}` } };
-// Bearer ${MyToken}沒有打空格就會他馬的吃不到  postman上右側可以讀取程式碼
 let signUp = {
     "Name":"",
     "Phone":"",
     "Password":"",
     "Email":"",
 };
-let signUpDate = [];  //先在全域宣告要儲存在axios載入後 要存放的位置
+let signUpDate = [];
 
-
-
-//getElementById
 
 const userName=document.querySelector(".userName");
 const userPhone=document.querySelector(".userPhone");
 const userEmail=document.querySelector(".userEmail");
 const userLogin=document.querySelector(".userLogin");
 const confirmPassword=document.querySelector(".confirmPassword");
-
 const signUpBtn=document.querySelector(".signUpBtn");
-
-
 
 function try02(){
     alert("彈跳測試");
@@ -35,6 +28,7 @@ function try02(){
     }
 
     console.log("yoooo",signUp); 
+
     axios.post(API,signUp)
     .then(function (response) {
         console.log(response);
